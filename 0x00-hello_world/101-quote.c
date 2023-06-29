7112fc7bb7471f6467ebcfaf274e11821da6927a
adegoke <mickymike177@gmail.com>
2023-06-28 20:15:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
- * Return 1 (Success)
+ * Return 1(Success)
  */
 int main(void)
 {
-fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n",stdout);
+fputs("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
